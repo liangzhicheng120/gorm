c4569d28f8e608bbea2b9b24b8a0770719118b5d
Zhicheng Liang <zhicheng.liang@shopee.com>
2022-09-18 19:56:30 +0800

    feature(example): init example 01

--- example/example_01.go
+++ example/example_01.go
@@ -114,10 +114,13 @@ func CreateDbClient() *gorm.DB {
 	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
 	sqlDB.SetMaxIdleConns(10)
 
-	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	// SetMaxIdleConns 设置空闲连接池中连接的最大生存时间
+	sqlDB.SetConnMaxIdleTime(time.Hour)
+
+	// SetMaxOpenConns 设置打开数据库连接的最大数量
 	sqlDB.SetMaxOpenConns(100)
 
-	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	// SetConnMaxLifetime 设置了连接可复用的最大时间
 	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	//PostgreSQL
